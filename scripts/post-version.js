fe325fdab0b3c32a650613068525a4a4ba955da3
tangshuang <mail@tangshuang.net>
2024-03-19 23:33:05 +0800

    feat: update version

--- scripts/post-version.js
+++ scripts/post-version.js
@@ -3,13 +3,9 @@ const path = require('path');
 const fs = require('fs');
 
 const root = path.resolve(__dirname, '..');
-
-execSync(`cd "${root}" && npm run release`);
-
 const text = fs.readFileSync(path.resolve(root, 'package.json')).toString();
 const json = JSON.parse(text);
 const { version } = json;
-
 const pkgsRoot = path.resolve(root, 'packages');
 const pkgs = fs.readdirSync(pkgsRoot);
 
