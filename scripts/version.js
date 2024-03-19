764b3f6e343fcf88a743d452da6636ebcaa16b46
tangshuang <mail@tangshuang.net>
2024-03-19 22:58:54 +0800

    ci: version

--- scripts/version.js
+++ scripts/version.js
@@ -1,10 +1,10 @@
-const { exec } = require('child_process');
+const { execSync, exec } = require('child_process');
 const path = require('path');
 const fs = require('fs');
 
 const root = path.resolve(__dirname, '..');
 
-exec(`cd "${root}" && npm run release`);
+execSync(`cd "${root}" && npm run release`);
 
 const text = fs.readFileSync(path.resolve(root, 'package.json')).toString();
 const json = JSON.parse(text);
