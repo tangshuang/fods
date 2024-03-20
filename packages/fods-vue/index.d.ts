ca2dec9e4e1286001d6c8e98cc00b71d54f8f275
tangshuang <mail@tangshuang.net>
2024-03-20 09:08:38 +0800

    fix: fix vue d.ts

--- packages/fods-vue/index.d.ts
+++ packages/fods-vue/index.d.ts
@@ -1,21 +1,22 @@
 import { Source, Composition } from 'fods';
+import { Ref } from 'vue';
 
 declare function useSource<T, U extends any[]>(source: Source<T, U>, defaultValue: T): {
-  data: T;
-  initing: boolean;
-  empty: boolean;
-  refreshing: boolean;
-  error: Error | null;
+  data: Ref<T>;
+  initing: Ref<boolean>;
+  empty: Ref<boolean>;
+  refreshing: Ref<boolean>;
+  error: Ref<Error | null>;
   init: (...params: U) => Promise<T>;
   refresh: () => Promise<T>;
 }
 
 declare function useSource<T, U>(source: Composition<T, U>, defaultValue: T[]): {
-  data: T[];
-  initing: boolean;
-  empty: boolean;
-  refreshing: boolean;
-  error: Error | null;
+  data: Ref<T[]>;
+  initing: Ref<boolean>;
+  empty: Ref<boolean>;
+  refreshing: Ref<boolean>;
+  error: Ref<Error | null>;
   init: (params: U[]) => Promise<T[]>;
   refresh: () => Promise<T[]>;
 };
